5a8736a357a73e0e8bd3bb0e181dbe229a506313
Kevin Li <kmli@mit.edu>
2017-05-15 00:04:15 -0400

    added file for reading data and converting to numpy matrices

---
+++ read_timeseries.py
@@ -0,0 +1,27 @@
+import numpy as np
+import networkx as nx
+import datetime
+import csv
+
+G = nx.read_gexf('network.gexf')
+
+indices = dict([(name, i) for i, name in enumerate(G.nodes())])
+max_interval = 1000
+
+states = np.zeros((500, max_interval))
+
+start_dt = datetime.datetime(2015, 4, 1, 0, 0, 0)
+
+unfound = []
+with open('stories_mentioning_altright.csv', 'rt') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='\"')
+    for row in reader:
+        if row[5][0].isalpha():
+            continue
+        name = row[1]
+        dt = datetime.datetime.strptime(row[5], '%Y-%m-%d %H:%M:%S')
+        minute_since_start = int((dt - start_dt).seconds / 60)
+        if name in indices:
+            states[indices[name],minute_since_start:] = 1
+
+A = nx.to_numpy_matrix(G)
