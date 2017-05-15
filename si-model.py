b41e68253f8ece7aa73328e9bcefac50b7238faa
Kevin Li <kmli@mit.edu>
2017-05-15 15:21:59 -0400

    add real si model

--- si-model.py
+++ si-model.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 SOURCES = 500
 
-TIME_STEPS = int(16000 / 240)
+TIME_STEPS = int(20000 / 24)
 
 A = np.load('adjacency.npy')
 
@@ -26,12 +26,12 @@ A = np.load('adjacency.npy')
 # S[8] = [1,1,1,1,1,1,1,1,1,0]
 # S[9] = [1,1,1,1,1,1,1,1,1,1]
 
-S = np.load('states.npy')[::240,:]
-plt.plot(np.sum(S, axis=1))
+S = np.load('states.npy')[::24,:]
+#plt.plot(np.sum(S, axis=1))
 
 power = np.zeros((TIME_STEPS))
-
-p = 1
+power_rand = np.zeros((TIME_STEPS))
+p = 0.1
 
 #normalize by row ... WEIGHT A
 
@@ -41,17 +41,25 @@ A_importance = A*np.sum(A, axis=0)
 
 A_importance_weighted = A_importance/np.sum(A_importance, axis = 1).reshape((SOURCES,1))
 
+A_realsi = A/np.sum(A, axis=0)
+A_realsi[np.isnan(A_realsi)] = 0
 
 for t in range(1, TIME_STEPS-2):
     #print('actual state', S[t])
-    prediction = p*np.dot(A_weighted, S[t])
+    prediction = p*(1 - np.dot(A_realsi, S[t]))
+    #print(np.sum(prediction) / len(S[t]))
+    prediction_rand = p * np.ones((len(S[t])))
     #print('prediction', prediction)
     probabilities = (1-S[t])*(np.abs(prediction - (1 - S[t+1])))
+    probabilities_rand = (1-S[t])*(np.abs(prediction_rand - (1 - S[t+1])))
     #print(probabilities)
     power[t] = np.sum(np.log(probabilities[np.nonzero(probabilities)]))
-    print(power[t])
+    power_rand[t] = np.sum(np.log(probabilities_rand[np.nonzero(probabilities_rand)]))
+    #print(power[t])
     #print('probability correct prediction', probabilities)
 
+#plt.plot(power_rand[1:-2])
 plt.plot(power[1:-2])
+print(np.sum(power[1:-2]))
 plt.ylabel('Probability of Correct Prediction')
 plt.show()
